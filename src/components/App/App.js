f75812fda974f312b5f3f4d205498b762ace78e7
TwanMulder <twan.mulder@gmail.com>
2020-01-02 17:36:48 +0100

    Created header and dashboard components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React, { Fragment } from "react"
 
 import Dashboard from "../Dashboard/Dashboard"
+import Header from "../Header/Header"
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
+      <Header></Header>
       <Dashboard></Dashboard>
-    </div>
+    </Fragment>
   )
 }
 
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,19 @@
 import React, { Fragment } from "react"
 
+import Beer from "../../img/beer.svg"
+
 function Dashboard() {
   return (
-    <Fragment>
-      <h1>Total amount of beer(s) not drunk</h1>
-      <h4>4</h4>
-      <p>
-        <span>-</span>
-        <span>+</span>
-      </p>
-    </Fragment>
+    <div className="dashboard">
+      <div className="dashboard-card">
+        {/* <img src={Beer} alt="Tall beer glass, filled with beer"></img> */}
+        <h4>4</h4>
+        <p>
+          <span>-</span>
+          <span>+</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
