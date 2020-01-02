161f0c4b7420430e446947c7908f9970393ede92
TwanMulder <twan.mulder@gmail.com>
2020-01-02 19:21:47 +0100

    Setup working incrementer

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,20 +1,45 @@
-import React, { Fragment } from "react"
+import React from "react"
 
-import Beer from "../../img/beer.svg"
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
 
-function Dashboard() {
-  return (
-    <div className="dashboard">
-      <div className="dashboard-card">
-        {/* <img src={Beer} alt="Tall beer glass, filled with beer"></img> */}
-        <h4>4</h4>
-        <p>
-          <span>-</span>
-          <span>+</span>
-        </p>
+  decrementCounter = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 })
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  render() {
+    const COUNT = this.state.count
+    return (
+      <div className="dashboard">
+        <div className="dashboard-card">
+          <h2 className="card-amount">
+            {COUNT}
+            <sup>BEERS SAVED</sup>
+          </h2>
+          <div className="card-controls">
+            <div className="card-controlMinus" onClick={this.decrementCounter}>
+              -
+            </div>
+            <div className="card-controlPlus" onClick={this.incrementCounter}>
+              +
+            </div>
+          </div>
+          <p className="card-saved">
+            You've saved €<span>{(COUNT * 0.9583).toFixed(2)}</span> so far
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Dashboard
