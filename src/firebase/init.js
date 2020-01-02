64b1759966ec3dea161c810c9aef0d53ba0d89b3
TwanMulder <twan.mulder@gmail.com>
2020-01-02 20:18:51 +0100

    Crazy secret code

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,6 +1,12 @@
-import firebase from "firebase"
-import secretKey from "./secretKey"
+import firebase from "firebase/app"
+import "firebase/database"
 
-firebase.initializeApp({ ...secretKey })
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "beer-tracker-6b9c8.firebaseapp.com",
+  databaseURL: "https://beer-tracker-6b9c8.firebaseio.com",
+  projectId: "beer-tracker-6b9c8",
+  appId: "1:323442284612:web:b1e63d48a36226ffcac411",
+})
 
 export default firebase
